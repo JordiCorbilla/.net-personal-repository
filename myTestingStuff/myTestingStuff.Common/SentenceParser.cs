378c16cb9f2d03fcca71099fc666e6fb988887fa
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2017-09-03 19:47:17 +0100

    Add sentence parser

---
+++ myTestingStuff/myTestingStuff.Common/SentenceParser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace myTestingStuff.Common
+{
+    public class SentenceParser
+    {
+        private string _sentence { get; set; }
+        public SentenceParser(string sentence)
+        {
+            _sentence = sentence;
+        }
+
+        public List<string> Parse(List<Token> tokens)
+        {
+            List<string> words = new List<string>();
+            string sentence = _sentence;
+            foreach(Token token in tokens)
+            {
+                if (sentence.Length > 0)
+                {
+                    int index = sentence.IndexOf(token.Start) + token.Start.Length;
+                    sentence = sentence.Substring(index); //remove the part till the first token.
+                                                          //Find the end of the first token
+                    if (!token.TillEnd)
+                    {
+                        index = sentence.IndexOf(token.End) + token.End.Length;
+                        string word = sentence.Substring(0, index);
+                        words.Add(word.Trim());
+                        sentence = sentence.Substring(index); //remove the items and keep storing the sentence
+                    }
+                    else
+                    {
+                        string word = sentence;
+                        words.Add(word.Trim());
+                        return words;
+                    }
+                }
+            }
+            return words;
+        }
+    }
+}
---
+++ myTestingStuff/myTestingStuff.Common/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myTestingStuff.Common
+{
+    public class Token
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public bool TillEnd { get; set; }
+    }
+}
